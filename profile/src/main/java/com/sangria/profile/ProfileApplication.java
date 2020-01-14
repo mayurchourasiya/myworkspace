eadd354109ee5ba30f41e901527789adc20bf7cd
mayurchourasiya <30589146+mayurchourasiya@users.noreply.github.com>
2020-01-15 00:59:00 +0530

    Fixed all issues

--- profile/src/main/java/com/sangria/profile/ProfileApplication.java
+++ profile/src/main/java/com/sangria/profile/ProfileApplication.java
@@ -1,10 +1,43 @@
 package com.sangria.profile;
 
+import com.sangria.profile.bean.Resume;
+import com.sangria.profile.service.ResumeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.math.BigDecimal;
+
+@RestController
 @SpringBootApplication
 public class ProfileApplication {
+
+	@Autowired
+	private ResumeService resumeService;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@PostMapping(value = "/create")
+	public ResponseEntity createRecord(@RequestBody Resume resume) {
+		BigDecimal profileId = resumeService.createProfile(resume.getProfile());
+		BigDecimal resumeId = resumeService.createResume(resume,profileId);
+		return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentContextPath().path("/"+resumeId.intValue()).build().toUri()).build();
+	}
+
+	@GetMapping(value = "/retrieve")
+	public ResponseEntity getRecord() {
+		return ResponseEntity.ok().build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfileApplication.class, args);
--- profile/src/main/java/com/sangria/profile/service/ResumeService.java
+++ profile/src/main/java/com/sangria/profile/service/ResumeService.java
@@ -1,19 +1,58 @@
 package com.sangria.profile.service;
 
+import com.sangria.profile.bean.Profile;
 import com.sangria.profile.bean.Resume;
-import com.sangria.profile.dao.ResumeDao;
+import com.sangria.profile.builder.Utility;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Map;
 
 @Component
 public class ResumeService {
 
     @Autowired
-    private ResumeDao resumeDao;
+    private JdbcTemplate jdbcTemplate;
 
-    public Integer createResume(Resume resume) {
-        int profileID = resumeDao.createProfile(resume.getProfile());
-        return resumeDao.createResume(resume,profileID);
+    private SimpleJdbcCall simpleJdbcCall;
+
+    @Autowired
+    private Utility utility;
+
+    /**
+     * First create the profile and generate the profileID to required to create resume
+     * @param profile
+     * @return profileID
+     */
+    public BigDecimal createProfile(Profile profile) {
+        simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate.getDataSource()).withCatalogName("RESUME_PKG").withProcedureName("createProfile");
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource().addValue("i_name",profile.getName())
+                .addValue("i_role",profile.getRole());
+        Map<String,Object> result = simpleJdbcCall.execute(sqlParameterSource);
+        return (BigDecimal) result.get("O_PROFILE_ID");
+    }
+
+
+    /**
+     * Get profileID and create resume and generate resumeID
+     * @param resume
+     * @param profileId
+     * @return resumeID
+     */
+    public BigDecimal createResume(Resume resume, BigDecimal profileId) {
+        String skillSet = utility.prepareSkillSet(resume.getSkillSet());
+        String hobby = utility.prepareHobbies(resume.getHobbies());
+        simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate.getDataSource()).withCatalogName("RESUME_PKG").withProcedureName("createResume");
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource().addValue("i_profile_id",profileId)
+                .addValue("i_skills",skillSet)
+                .addValue("i_hobbies",hobby);
+        Map<String,Object> result = simpleJdbcCall.execute(sqlParameterSource);
+        return (BigDecimal) result.get("O_RESUME_ID");
     }
 
 }
