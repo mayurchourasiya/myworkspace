3e32e924755761fa2e40f7eb746159e48d25fe4d
mayurchourasiya <30589146+mayurchourasiya@users.noreply.github.com>
2020-01-15 13:31:00 +0530

    Update dbScripts.sql

--- profile/src/main/resources/dbScripts.sql
+++ profile/src/main/resources/dbScripts.sql
@@ -11,9 +11,48 @@ create table RESUME_RECORDS (RESUME_ID NUMBER(20) DEFAULT resume_seq.nextval NOT
 ***************************************************************************************************
 
 //3. create procedures
+create or replace PACKAGE RESUME_PKG IS
+
+PROCEDURE createResume(
+i_profile_id IN VARCHAR2,
+i_skills IN VARCHAR2,
+i_hobbies IN VARCHAR2,
+o_resume_id OUT NUMBER
+);
+PROCEDURE createProfile(
+i_name IN VARCHAR2,
+i_role IN VARCHAR2,
+o_profile_id OUT NUMBER
+);
+PROCEDURE getProfile(
+i_profile_id IN NUMBER,
+o_name OUT VARCHAR2,
+o_role OUT VARCHAR2
+);
+PROCEDURE getResume(
+i_resume_id IN NUMBER,
+o_profile_id OUT NUMBER,
+o_skills OUT VARCHAR2,
+o_hobbies OUT VARCHAR2
+);
+----------------------------
+/* update */
+PROCEDURE updateResume(
+i_resume_id IN NUMBER,
+i_skills IN VARCHAR2,
+i_hobbies IN VARCHAR2,
+i_name IN VARCHAR2,
+i_role IN VARCHAR2,
+o_resume_id OUT NUMBER
+);
+END RESUME_PKG;
+*****************************************************************************************************
+//4. write queries to insert the records
+
 create or replace PACKAGE BODY RESUME_PKG IS
 
 /*create the resume*/
+
 PROCEDURE createResume(
 i_profile_id IN VARCHAR2,
 i_skills IN VARCHAR2,
@@ -85,64 +124,3 @@ select resume_id into o_resume_id from RESUME_RECORDS where PROFILE_ID = v_profi
 end updateResume;
 
 END RESUME_PKG;
-
-*****************************************************************************************************
-//4. write queries to insert the records
-
-create or replace PACKAGE BODY RESUME_PKG IS
-PROCEDURE createResume(
-i_profile_id IN VARCHAR2,
-i_skills IN VARCHAR2,
-i_hobbies IN VARCHAR2,
-o_resume_id OUT NUMBER
-)
-IS
-BEGIN
-insert into RESUME_RECORDS(SKILLSET,HOBBIES,PROFILE_ID) values(i_skills,i_hobbies,i_profile_id) RETURNING RESUME_ID into o_resume_id;
-commit;
-end createResume;
--------------------------------------------------------------------------------------
-/*First create the profile*/
-PROCEDURE createProfile(
-i_name IN VARCHAR2,
-i_role IN VARCHAR2,
-o_profile_id OUT NUMBER
-)
-IS
-BEGIN
-insert into PROFILES(name,role) values(i_name,i_role) RETURNING PROFILE_ID into o_profile_id;
-commit;
-end createProfile;
-
-PROCEDURE getProfile(
-i_profile_id IN NUMBER,
-o_name OUT VARCHAR2,
-o_role OUT VARCHAR2
-)
-IS
-BEGIN
-select name,role into o_name,o_role from PROFILES;
-end getProfile;
-
-PROCEDURE getResume(
-i_resume_id IN NUMBER,
-o_profile_id OUT NUMBER,
-o_skills OUT VARCHAR2,
-o_hobbies OUT VARCHAR2
-)
-IS
-BEGIN
-select profile_id,SKILLSET,hobbies into o_profile_id,o_skills,o_hobbies from RESUME_RECORDS;
-end getResume;
-----------------------------
-/* update */
-PROCEDURE updateResume(
-i_resume_id IN NUMBER,
-i_skills IN VARCHAR2,
-i_hobbies IN VARCHAR2,
-i_name IN VARCHAR2,
-i_role IN VARCHAR2,
-o_resume_id OUT NUMBER
-);
-END RESUME_PKG;
-*****************************************************************************
